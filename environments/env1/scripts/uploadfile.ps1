c8353cb3c9bf2530632483b1097918e767918456
vAyushkhare <110239762+vAyushkhare@users.noreply.github.com>
2022-10-07 11:21:29 +0530

    Update uploadfile.ps1

--- environments/env1/scripts/uploadfile.ps1
+++ environments/env1/scripts/uploadfile.ps1
@@ -2,7 +2,7 @@ param(
 [string] $ResourceGroupName,
 [string] $RawDataLakeAccountName
 )
-Write-Host "file upload script called"
+Write-Host "file upload script started"
  if ((Get-Module -ListAvailable Az.Accounts) -eq $null)
 	{
        Install-Module -Name Az.Accounts -Force
@@ -17,3 +17,8 @@ $ctx = New-AzStorageContext -StorageAccountName $RawDataLakeAccountName -Storage
 
 Invoke-WebRequest -Uri $uri -OutFile $bacpacFileName
 Set-AzStorageBlobContent -File $bacpacFileName -Container "raw" -Blob 'import-terms-sample.csv' -Context $ctx
+
+Write-Host "file upload script completed"
+$uri1 = "https://raw.githubusercontent.com/CSALabsAutomation/azure-purview-labs/main/environments/env1/Artifacts
+Invoke-WebRequest -Uri $uri
+Get-ChildItem -File -Recurse | Set-AzStorageBlobContent -Container "raw" -Context $ctx
