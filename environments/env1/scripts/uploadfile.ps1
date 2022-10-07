b6751d532b62e00ede5946e573da1946d7cb1517
Nivedha G <109720688+Nivedha-govindan@users.noreply.github.com>
2022-10-07 14:23:24 +0530

    Update uploadfile.ps1

--- environments/env1/scripts/uploadfile.ps1
+++ environments/env1/scripts/uploadfile.ps1
@@ -21,15 +21,27 @@ Set-AzStorageBlobContent -File $bacpacFileName -Container "raw" -Blob 'import-te
 
 #############################
 
-$uri = "https://raw.githubusercontent.com/CSALabsAutomation/azure-purview-labs/main/environments/env1/Artifacts/import-terms-sample.csv";
-$bacpacFileName = "import-terms-sample.csv";
+$uri = "https://raw.githubusercontent.com/CSALabsAutomation/azure-purview-labs/main/environments/env1/Artifacts/Twitter/twitter_handles.parquet";
+$bacpacFileName = "twitter_handles.parquet";
 
 $storageaccountkey = Get-AzStorageAccountKey -ResourceGroupName $Resourcegroupname -Name $RawDataLakeAccountName;
 
 $ctx = New-AzStorageContext -StorageAccountName $RawDataLakeAccountName -StorageAccountKey $storageaccountkey.Value[0]
 
 Invoke-WebRequest -Uri $uri -OutFile $bacpacFileName
-Set-AzStorageBlobContent -File $bacpacFileName -Container "raw" -Blob 'twitter/import-terms-sample.csv' -Context $ctx
+Set-AzStorageBlobContent -File $bacpacFileName -Container "raw" -Blob 'Twitter/twitter_handles.parquet' -Context $ctx
+
+#############################
+
+$uri = "https://raw.githubusercontent.com/CSALabsAutomation/azure-purview-labs/main/environments/env1/Artifacts/Twitter/twitter_handles.csv";
+$bacpacFileName = "twitter_handles.csv";
+
+$storageaccountkey = Get-AzStorageAccountKey -ResourceGroupName $Resourcegroupname -Name $RawDataLakeAccountName;
+
+$ctx = New-AzStorageContext -StorageAccountName $RawDataLakeAccountName -StorageAccountKey $storageaccountkey.Value[0]
+
+Invoke-WebRequest -Uri $uri -OutFile $bacpacFileName
+Set-AzStorageBlobContent -File $bacpacFileName -Container "raw" -Blob 'Twitter/twitter_handles.csv' -Context $ctx
 
 
 Write-Host "file upload script completed"
