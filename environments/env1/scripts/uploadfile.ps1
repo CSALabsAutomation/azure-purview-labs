3927ca4e8b4d6670edec8a740c4124b89d569c50
vAyushkhare <110239762+vAyushkhare@users.noreply.github.com>
2022-10-07 13:28:52 +0530

    Update uploadfile.ps1

--- environments/env1/scripts/uploadfile.ps1
+++ environments/env1/scripts/uploadfile.ps1
@@ -19,6 +19,24 @@ Invoke-WebRequest -Uri $uri -OutFile $bacpacFileName
 Set-AzStorageBlobContent -File $bacpacFileName -Container "raw" -Blob 'import-terms-sample.csv' -Context $ctx
 
 Write-Host "file upload script completed"
-$uri1 = "https://raw.githubusercontent.com/CSALabsAutomation/azure-purview-labs/main/environments/env1/Artifacts
-Invoke-WebRequest -Uri $uri
-Get-ChildItem -File -Recurse | Set-AzStorageBlobContent -Container "raw" -Context $ctx
+
+Write-Host "R file upload script started"
+$uri = "https://github.com/CSALabsAutomation/azure-purview-labs/tree/main/environments/env1/Artifacts/Twitter";
+$bacpacFileName = "import-terms-sample.csv";
+$storageaccountkey = Get-AzStorageAccountKey -ResourceGroupName $Resourcegroupname -Name $RawDataLakeAccountName;
+New-Item -Path 'C:\tempGitFiles' -ItemType Directory
+$dir = 'C:\tempGitFiles'
+Set-Location $dir
+$ctx = New-AzStorageContext -StorageAccountName $RawDataLakeAccountName -StorageAccountKey $storageaccountkey.Value[0]
+$files = 'twitter_handles.csv',
+          'twitter_handles.parquet';
+          foreach ($file in $files)
+{
+   Write-Host "Downloading $file"
+   Write-Host $dlUrl
+    Write-Host $dlPath
+   $dlUrl = "$($uri)/$file"
+   $dlPath = "$($dir)/$file"
+   Invoke-WebRequest -Uri $dlUrl -OutFile $dlPath
+}
+Get-ChildItem -File -Recurse | Set-AzStorageBlobContent -Container "raw" -Blob '' -Context $ctx
